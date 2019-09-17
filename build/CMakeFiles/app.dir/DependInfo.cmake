03d64a12df53dd2149824dcf99b5461897c69db5
Martin-linux <cinekqw1@gmail.com>
2019-09-17 09:14:10 +0200

    add git ingore

--- build/CMakeFiles/app.dir/DependInfo.cmake
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -4,10 +4,10 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/marcin/Programming/c++/message_app/src/AppAgent.cpp" "/Users/marcin/Programming/c++/message_app/build/CMakeFiles/app.dir/src/AppAgent.cpp.o"
-  "/Users/marcin/Programming/c++/message_app/src/main.cpp" "/Users/marcin/Programming/c++/message_app/build/CMakeFiles/app.dir/src/main.cpp.o"
+  "/home/choroman/MY_PROJECTS/message_app/src/AppAgent.cpp" "/home/choroman/MY_PROJECTS/message_app/build/CMakeFiles/app.dir/src/AppAgent.cpp.o"
+  "/home/choroman/MY_PROJECTS/message_app/src/main.cpp" "/home/choroman/MY_PROJECTS/message_app/build/CMakeFiles/app.dir/src/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
