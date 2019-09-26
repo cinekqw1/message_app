db116797643e78c9b44cd1393d0afe7bc4ac948f
Martin-linux <cinekqw1@gmail.com>
2019-09-26 11:05:46 +0200

    re-build logic

--- src/AppAgent.cpp
+++ src/AppAgent.cpp
@@ -17,11 +17,12 @@ AppAgent::AppAgent()
 void AppAgent::run()
 {
 
-	std::exception_ptr thd_exception;
+	std::exception_ptr thd_exception_1;
+	std::exception_ptr thd_exception_2;
 
 
-	Thread_1 thred_1(2,4, std::ref(thd_exception));
-	Thread_2 thred_2(4,8, std::ref(thd_exception));
+	Thread_1 thred_1(2,4, std::ref(thd_exception_1));
+	Thread_2 thred_2(4,8, std::ref(thd_exception_2));
 	
 	std::thread t1(thred_1);
 	std::thread t2(thred_2);
@@ -29,42 +30,28 @@ void AppAgent::run()
 	t1.detach(); 
 	t2.detach(); 
 
-	std::queue<std::exception_ptr> l_exceptions;
 
 	while (true)
 	{
 
-		if(thd_exception)
-		{
-			l_exceptions.push(thd_exception);
-			LOG("pushed");	
-		}
-		if( !l_exceptions.empty() && l_exceptions.back() != thd_exception)
-		{
-			l_exceptions.push(thd_exception);
-			LOG("pushed");	
-		}
-
 		try {
-	        if (!l_exceptions.empty()) {
-	        	std::exception_ptr l_exception = l_exceptions.front();
-	        	l_exceptions.pop();
-	            std::rethrow_exception(l_exception);
-	            
-	        }
+			
+	        if (thd_exception_1) std::rethrow_exception(thd_exception_1);
+	        if (thd_exception_2) std::rethrow_exception(thd_exception_2);
+	        
 	    } catch(const std::exception& e) {
 	        std::cout << "Caught exception \"" << e.what() << "\"\n";
 	        if(strcmp(e.what(), "my_err_thread_1")== 0)
-	        {	
+	        {	thd_exception_1 = nullptr;
 	        	std::thread t1(thred_1);
 	       		t1.detach();
 	        }
 	        if(strcmp(e.what(), "my_err_thread_2")== 0)
 	        {
+	        	thd_exception_2 = nullptr;
 	        	std::thread t2(thred_2);
 	        	t2.detach();
 	        }
-	        thd_exception = nullptr;
     }
 
 	}
